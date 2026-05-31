b299ea3f1a49a9f5e99297b79b65363e31922334
officetomo1121 <info@office-tomo.net>
2026-05-31 19:18:28 +0900

    2026/05/31 updated

--- app/components/Top.vue
+++ app/components/Top.vue
@@ -9,14 +9,14 @@
         <div>
           <label for="email" class="block text-sm/6 font-medium text-black">Email address</label>
           <div class="mt-2">
-            <input type="email" name="email" id="email" autocomplete="email" required="" class="border border-indigo-100 block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input v-model="email" type="email" name="email" id="email" autocomplete="email" required="" class="border border-indigo-100 block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
           </div>
         </div>
 
         <div>
           <label for="password" class="block text-sm/6 font-medium text-black">Password</label>
           <div class="mt-2">
-            <input type="password" name="password" id="password" autocomplete="current-password" required="" class="border border-indigo-100 block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            <input v-model="pass" type="password" name="password" id="password" autocomplete="current-password" required="" class="border border-indigo-100 block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
           </div>
         </div>
 
