5deab1c1c559e0e8940d0b078377c0755f7f4fca
officetomo1121 <info@office-tomo.net>
2026-06-06 16:08:12 +0900

    2026/06/06 updated

--- app/pages/sender.vue
+++ app/pages/sender.vue
@@ -12,7 +12,7 @@
         v-model="detail"
         :extensions="[ImageUpload,
                       TextAlign.configure({
-                        types: ['heading', 'paragraph']
+                        types: ['paragraph', 'heading']
                       })
                      ]"
         :handlers="customHandlers"
@@ -20,6 +20,11 @@
         :ui="{ base: 'p-8 sm:px-16' }"
         class="w-full min-h-74 border border-slate-300 shadow-lg mb-3"
       >
+        <UEditorToolbar
+          :editor="editor"
+          :items="items"
+          class="border-b border-muted py-2 px-8 sm:px-16 overflow-x-auto"
+        />
         <UEditorDragHandle :editor="editor" />
         <UEditorSuggestionMenu :editor="editor" :items="items" :append-to="appendToBody" />
       </UEditor>
